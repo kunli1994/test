bbb817448260de8cec2c1133a2eea25a895385be
Kun Li <kunli@Kuns-MacBook-Pro.local>
2018-11-10 17:23:08 -0500

    minor change

--- app/test.py
+++ app/test.py
@@ -11,5 +11,5 @@ def hello():
         return "Hello World!\n"
 
 if __name__ == "__main__":
-    app.run(port=os.environ.get("PORT", "5000"))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
 
